e4615f5221da81deaadf175f7bc1f358cfafaf2a
Ting-Ying Wu <tingying.wu@gmail.com>
2022-11-07 22:49:02 +0800

    Add comments

--- cartographer_ros/cartographer_ros/pbstream_to_ros_map_main.cc
+++ cartographer_ros/cartographer_ros/pbstream_to_ros_map_main.cc
@@ -69,6 +69,7 @@ void Run(const std::string& pbstream_filename, const std::string& map_filestem,
 }  // namespace cartographer_ros
 
 int main(int argc, char** argv) {
+  // Initialize
   FLAGS_alsologtostderr = true;
   google::InitGoogleLogging(argv[0]);
   google::ParseCommandLineFlags(&argc, &argv, true);
